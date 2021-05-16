6a806f5702fe8c0e0734756a239b40e6d1a05f70
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-05-16 12:05:38 -0600

    commit tree 2834d96eee2919957ad91ad65d7490a66e4ad5b3

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Person
   #your code here
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
@@ -5,6 +7,7 @@ class Person
   def initialize(attributes = nil)
     if attributes 
       attributes.each {|key, value| self.send("#{key}=", value)}
+      # binding.pry
     end
   end 
 end
